a97473ebc49d2d56748f17166307b333f652c23c
LuckyPants27 <hitori-a@yandex.ru>
2023-01-11 04:39:25 +0500

    Удалил закоменнтированный код

--- src/main/java/com/vadimgrishenkov/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
+++ src/main/java/com/vadimgrishenkov/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
@@ -17,12 +17,6 @@ public class EmployeeDAOImpl  implements EmployeeDAO{
     @Override
     public List<Employee> getAllEmployees() {
 
-//        Session session = entityManager.unwrap(Session.class);
-//
-//        Query<Employee> query = session.createQuery("from Employee"
-//                , Employee.class);
-//        List<Employee> allEmployees = query.getResultList();
-
         Query query = entityManager.createQuery("from Employee");
         List<Employee> allEmployees = query.getResultList();
 
@@ -32,10 +26,6 @@ public class EmployeeDAOImpl  implements EmployeeDAO{
     @Override
     public void saveEmployee(Employee employee) {
 
-//        Session session = entityManager.unwrap(Session.class);
-//
-//        session.saveOrUpdate(employee);
-
         Employee newEmployee = entityManager.merge(employee);
         employee.setId(newEmployee.getId());
 
@@ -44,22 +34,12 @@ public class EmployeeDAOImpl  implements EmployeeDAO{
     @Override
     public Employee getEmployee(int id) {
 
-//        Session session = entityManager.unwrap(Session.class);
-//
-//        Employee employee = session.get(Employee.class, id);
-
         Employee employee = entityManager.find(Employee.class, id);
         return employee;
     }
 
     @Override
     public void deleteEmployee(int id) {
-
-//        Session session = entityManager.unwrap(Session.class);
-//        Query<Employee> query = session.createQuery("delete from Employee " +
-//                "where id =:employeeId");
-//        query.setParameter("employeeId", id);
-//        query.executeUpdate();
 
         Query query = entityManager.createQuery("delete from Employee " +
                 "where id =:employeeId");
